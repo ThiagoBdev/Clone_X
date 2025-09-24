02ff2dd5f1e1e2b406592faad0a41ac5972ad447
thiago batista <thiagobatista15390@gmail.com>
2025-09-23 21:42:30 -0300

    retirando o cors true

--- back/backend/settings.py
+++ back/backend/settings.py
@@ -55,8 +55,6 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
-
 CORS_ALLOWED_ORIGINS = [
     "https://clone-x-three.vercel.app", "http://localhost:3000",
 ]
