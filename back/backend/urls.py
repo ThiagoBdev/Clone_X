c3e69f7a7de0fc0f50150071c5a069342b224e0d
thiago batista <thiagobatista15390@gmail.com>
2025-09-23 21:29:00 -0300

    corrigindo urls

--- back/backend/urls.py
+++ back/backend/urls.py
@@ -15,6 +15,5 @@ urlpatterns = [
     path('api/', include(router.urls)),  
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/users/', include('users.urls')),
     path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT}),  
 ]
